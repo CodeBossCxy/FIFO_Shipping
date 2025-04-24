40d70188a01234d0dd9966b086dc3fd100f7a844
CodeBossCxy <iamcaitlyn0531@gmail.com>
2025-04-24 16:56:31 -0400

    4/24

--- static/shipper_containers.js
+++ static/shipper_containers.js
@@ -1,5 +1,6 @@
 var container_to_scan = [];
 var building_code = "";
+
 async function load_shipper_containers() {
     console.log("Successfully loaded");
     const shipper_number = document.querySelector('h1').textContent;
@@ -104,46 +105,59 @@ function simulateScan(value) {
 // simulateScan("ABC12345");  // Call this in console to simulate a scan
 
 async function get_master_containers(master_unit_no) {
-    // console.log("[get_master_containers] master_unit_no: ", master_unit_no);
-    // fetch(`/check/master_containers`,
-    //     {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             master_unit_no: master_unit_no
-    //         })
-    //     }
-    // ).then(response => response.json()).then(data => {
-    //     console.log("data: \n", data);
-    // //     
-    // })
-    // .catch(error => {
-    //     console.error('Error updating serial no:', error);
-    // });
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Basic VmludGVjaFdTQHBsZXguY29tOmE0Y2Q3OGEtZmUxMi00");
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-        "inputs": {
-        "Master_Unit_No": master_unit_no
+    console.log("[get_master_containers] master_unit_no: ", master_unit_no);
+    await fetch(`/check/master_containers`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                master_unit_no: master_unit_no
+            })
         }
+    ).then(response => response.json()).then(data => {
+        console.log("data: \n", data);
+        return data;
+    //     
+    })
+    .catch(error => {
+        console.error('Error updating serial no:', error);
     });
 
-    const requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-        redirect: "follow"
-    };
 
-    fetch("https://Vintech.on.plex.com/api/datasources/12934/execute", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
-    }
+
+    // const myHeaders = new Headers();
+    // myHeaders.append("Authorization", "Basic VmludGVjaFdTQHBsZXguY29tOmE0Y2Q3OGEtZmUxMi00");
+    // myHeaders.append("Content-Type", "application/json");
+
+    // const raw = JSON.stringify({
+    //     "inputs": {
+    //     "Master_Unit_No": master_unit_no
+    //     }
+    // });
+
+    // const requestOptions = {
+    //     method: "POST",
+    //     headers: myHeaders,
+    //     body: raw,
+    //     redirect: "follow"
+    // };
+    // try {
+    //     result = await fetch("https://Vintech.on.plex.com/api/datasources/12934/execute", requestOptions)
+    //     if (!result.ok) {
+    //         throw new Error('Network response was not ok');
+    //     }
+    //     const data = await result.json();
+    //     console.log("data: ", data);
+    //     return data;
+    // } catch (error) {
+    //     console.error('Error getting master unit:', error);
+    // }
+    
+
+    // return result;
+}
 
 
 async function update_master_unit(master_unit_no) {
@@ -173,15 +187,20 @@ async function send_scanner_input() {
         scanTimeout = setTimeout(async () => {
             console.log("scanBuffer: ", scanBuffer);
             if (scanBuffer[0] == "M"){
-                containers = await get_master_containers(scanBuffer)
-                containers.forEach(container => {
+                try {
+                    const container_list = await get_master_containers(scanBuffer)
+                    console.log("container_list: ", container_list.containers);
+                    container_list.containers.forEach(container => {
                     if (!container_to_scan.includes(container)) {
                         alert("Invalid container, violating FIFO policy");
                     }
                     else {
                         update_master_unit(scanBuffer);
-                    }
-                })
+                        }
+                    })
+                } catch (error) {
+                    console.error('Error getting master unit:', error);
+                }
             }
             else if (container_to_scan.includes(scanBuffer)) {
                 console.log("available container scanned: ",scanBuffer);
